859c04a7277b39db32f4743a11dcbb29ff8b8d30
sonagahraman <114945287+sonagahraman@users.noreply.github.com>
2022-11-17 18:38:15 +0400

    Update main.js

--- js-1-func/main.js
+++ js-1-func/main.js
@@ -55,7 +55,7 @@
 // }
 // resultfnc();
 // Task 5 
-// function quadraticEquation(){
+// function quadratic(){
 //     let a = +prompt("Enter A: ");
 //     let b = +prompt("Enter B: ");
 //     let c = +prompt("Enter C: ");
@@ -73,7 +73,7 @@
 //       console.log(`The roots are real`);
 //     }
 // }
-// quadraticEquation();
+// quadratic();
 
 //Task7
 function radius();{
